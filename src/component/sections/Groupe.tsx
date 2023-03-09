c813be2656b36d20557b3b5d92a2ae4b595469dd
qusewen <quse.quse@mail.ru>
2023-03-09 21:40:07 +0300

    feat: add groupe section

--- src/component/sections/Groupe.tsx
+++ src/component/sections/Groupe.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import Calendar from "../../assets/svg/calendar.svg";
+import Location from "../../assets/svg/location.svg";
+import People from "../../assets/svg/people.svg";
 export default function Groupe() {
   return (
-    <div>Groupe</div>
-  )
+    <section className='groupe'>
+      <div className='container'>
+        <div className='groupe__body'>
+          <p className='groupe__subtitle'>Онлайн запись на групповой тренинг</p>
+          <h2 className='groupe__title'>Онлайн группа для людей с РПП</h2>
+          <div className='groupe__info'>
+            <p className='groupe__subtitle-info'>
+              <img src={Calendar} alt='calendar' className='groupe__img' />
+              Еженедельно, старт в АПРЕЛЕ
+            </p>
+            <p className='groupe__subtitle-info'>
+              <img src={Location} alt='location' className='groupe__img' />
+              Онлайн
+            </p>
+          </div>
+          <p className='groupe__subtitle-info people'>
+            <img src={People} alt='location' className='groupe__img' />
+            Колличество участников ограничено
+          </p>
+          <p className="groupe__subtitle">
+            Расстройство пищевого поведения - это заболевания, которые
+            представлены в международном классификаторе болезней.
+          </p>
+          <p className="groupe__subtitle">
+            Групповой формат работы в данной теме позволяет клиентам то, что
+            невозможно в личной терапии, познакомиться с людьми с такой же
+            проблемой, получить возможность говорить о своей проблеме и
+            понимать, что не одинок в своих переживаниях, и нет твоей вины в
+            том, что с тобой происходит, совершенствовать навыки общения с
+            Другими.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }

0a83122260b4a4759108fac5d3f587ce0ae3be5e
qusewen <quse.quse@mail.ru>
2023-03-09 22:50:21 +0300

    feat: add footer

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../../component/sections/About";
 import Groupe from "../../component/sections/Groupe";
+import Work from "../../component/sections/Work";
 
 import "./Main.scss";
 export default function Main() {
@@ -8,6 +9,7 @@ export default function Main() {
     <main className='main'>
       <About/>
       <Groupe/>
+      <Work/>
     </main>
   );
 }

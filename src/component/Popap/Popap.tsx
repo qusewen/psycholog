9f5d34bbdcbe14f1a7af1006282b3c507a4a0c2b
qusewen <quse.quse@mail.ru>
2023-03-13 18:20:12 +0300

    feat: create loader and errors

--- src/component/Popap/Popap.tsx
+++ src/component/Popap/Popap.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux/es/exports";
-import { send } from 'emailjs-com';
+import { send } from "emailjs-com";
 import "./Popap.scss";
+import Loader from "../Loader/Loader";
 interface States {
   modal: {
     modalSet: boolean;
@@ -13,24 +14,25 @@ export default function Popap() {
   const dispatch = useDispatch();
   const [status, setStatus] = useState(false);
   const [toSend, setToSend] = useState({
-    name: '',
-    phone: '',
-    comment: '',
-    service: 'Заявка на консультацию',
+    name: "",
+    phone: "",
+    comment: "",
+    service: "Заявка на консультацию",
   });
+  const [resStatus, setResStatus] = useState<number>();
+  const [errorStatus, setErrorStatus] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
   function modalStateClose() {
     dispatch({ type: "closeModal" });
   }
-useEffect(()=>{
-  if(valueStateModal.modalSet){
-    setStatus(true)
-  }else{
-    setStatus(false)
-  }
-},  [valueStateModal])
-  const {
-    register,
-  } = useForm({
+  useEffect(() => {
+    if (valueStateModal.modalSet) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  }, [valueStateModal]);
+  const { register } = useForm({
     defaultValues: {
       name: "",
       phone: "",
@@ -41,66 +43,79 @@ useEffect(()=>{
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    send(
-      'service_06f55b9',
-      'template_ky6046b',
-      toSend,
-      'G1_Vie_WUUG0AetTx'
-    )
+    setLoading(true);
+    send("service_06f55b9", "template_ky6046b", toSend, "G1_Vie_WUUG0AetTx")
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        setResStatus(response.status);
+        setLoading(false);
+        console.log("SUCCESS!", response.status, response.text);
       })
       .catch((err) => {
-        console.log('FAILED...', err);
+        setErrorStatus(true);
+        setLoading(false);
+        console.log("FAILED...", err);
       });
   };
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setToSend({ ...toSend, [e.target.name]: e.target.value });
   };
 
   return (
-
-    <div className={status? 'popap active__popap': 'popap'}>
+    <div className={status ? "popap active__popap" : "popap"}>
       <div className='popap__body'>
         <div className='exit' onClick={() => modalStateClose()}>
           X
         </div>
-        <form className='onlineRecord onlineRecord__popap' onSubmit={onSubmit}>
-          <h2 className='onlineRecord__title onlineRecord__title-popap'>
-            Введите данные для связи с вами
-          </h2>
-          <input
-            {...register("name")}
-            className='input popap_input'
-            type='text'
-            placeholder='Ваше имя'
-            name='name'
-            value={toSend.name}
-            onChange={handleChange}
-          />
-          <input
-            {...register("phone")}
-            className='input popap_input'
-            type='text'
-            placeholder='Номер телефона для связи'
-            name='phone'
-            value={toSend.phone}
-            onChange={handleChange}
-          />{" "}
-          <input
-            {...register("comment")}
-            className='input input__phone popap_input'
-            type='text'
-            placeholder='Ваш комментарий'
-            name='comment'
-            value={toSend.comment}
-            onChange={handleChange}
-          />
-          <button className='button' type='submit'>
-      Записаться
-    </button>
-        </form>
+        {loading ? <Loader clas='loader' /> : ""}
+        {resStatus === 200 ? (
+          <div className='success'>
+            Сообщение отправлено, скоро с вами свяжутся!
+          </div>
+        ) : errorStatus ? (
+          <div className='error'>
+            Произошла ошибка, обновите страницу и попробуйте заново
+          </div>
+        ) : (
+          <form
+            className='onlineRecord onlineRecord__popap'
+            onSubmit={onSubmit}
+          >
+            <h2 className='onlineRecord__title onlineRecord__title-popap'>
+              Введите данные для связи с вами
+            </h2>
+            <input
+              {...register("name")}
+              className='input popap_input'
+              type='text'
+              placeholder='Ваше имя'
+              name='name'
+              value={toSend.name}
+              onChange={handleChange}
+            />
+            <input
+              {...register("phone")}
+              className='input popap_input'
+              type='text'
+              placeholder='Номер телефона для связи'
+              name='phone'
+              value={toSend.phone}
+              onChange={handleChange}
+            />{" "}
+            <input
+              {...register("comment")}
+              className='input input__phone popap_input'
+              type='text'
+              placeholder='Ваш комментарий'
+              name='comment'
+              value={toSend.comment}
+              onChange={handleChange}
+            />
+            <button className='button' type='submit'>
+              Записаться
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
--- src/component/forms/Form-online-record.tsx
+++ src/component/forms/Form-online-record.tsx
@@ -1,6 +1,7 @@
 import { send } from "emailjs-com";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import Loader from "../Loader/Loader";
 import "./Form.scss";
 export default function FormOnlineRecord() {
   const { register } = useForm({
@@ -16,14 +17,22 @@ export default function FormOnlineRecord() {
     phone: "",
     service: "Онлайн группа РПП",
   });
-
+  const [resStatus, setResStatus] = useState<number>();
+  const [errorStatus, setErrorStatus] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true);
     event.preventDefault();
     send("service_06f55b9", "template_vwqox86", toSend, "G1_Vie_WUUG0AetTx")
       .then((response) => {
+        setLoading(false);
+        setErrorStatus(false);
+        setResStatus(response.status);
         console.log("SUCCESS!", response.status, response.text);
       })
       .catch((err) => {
+        setLoading(false);
+        setErrorStatus(true);
         console.log("FAILED...", err);
       });
   };
@@ -32,29 +41,43 @@ export default function FormOnlineRecord() {
   };
 
   return (
-    <form className='onlineRecord' onSubmit={onSubmit}>
-      <h2 className='onlineRecord__title'>Заполнить данные для записи</h2>
-      <input
-        {...register("name")}
-        className='input'
-        type='text'
-        placeholder='Ваше имя'
-        name='name'
-        value={toSend.name}
-        onChange={handleChange}
-      />
-      <input
-        {...register("phone")}
-        className='input input__phone'
-        type='text'
-        placeholder='Номер телефона для связи'
-        name='phone'
-        value={toSend.phone}
-        onChange={handleChange}
-      />
-      <button className='button' type='submit'>
-        Записаться
-      </button>
-    </form>
+    <div className='onlineRecord'>
+      {resStatus === 200 ? (
+        <div className='success_rec'>
+          Сообщение отправлено, скоро с вами свяжутся!
+        </div>
+      ) : errorStatus ? (
+        <div className='error_rec'>
+          Произошла ошибка, обновите страницу и попробуйте заново
+        </div>
+      ) : (
+        <form className='onlineRecord' onSubmit={onSubmit}>
+          <h2 className='onlineRecord__title'>Заполнить данные для записи</h2>
+
+          <input
+            {...register("name")}
+            className='input'
+            type='text'
+            placeholder='Ваше имя'
+            name='name'
+            value={toSend.name}
+            onChange={handleChange}
+          />
+          <input
+            {...register("phone")}
+            className='input input__phone'
+            type='text'
+            placeholder='Номер телефона для связи'
+            name='phone'
+            value={toSend.phone}
+            onChange={handleChange}
+          />
+          <button className='button' type='submit'>
+            Записаться
+          </button>
+          {loading ? <Loader clas='loader-rec' /> : ""}
+        </form>
+      )}
+    </div>
   );
 }
